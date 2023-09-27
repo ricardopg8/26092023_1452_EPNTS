a593d44262b368f2fc2626268b9cff10951e4a30
Ricardo Gonçalves <ricardo.goncalves@bit.com.br>
2023-09-27 12:00:02 -0300

    Addted extentions to writing in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { server } from './server/server';
-
+// eslint-disable-next-line linebreak-style
+import { server } from './server/server.1';
 server.listen(3333, () => console.log('App rodando!'));
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,2 @@
-import express from 'express';
 
 
-
-const server = express();
-
-
-server.get('/', (req, res) => {
-    return res.send('Olá, DEV!');
-} );
-
-
-export { server };
